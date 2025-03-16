f189a177ce7b87a05935202d27e4b43a559df5d5
lam.bui <lam.bui@cloudgo.vn>
2025-03-16 20:49:32 +0700

    Update parent node's data from child node

---
+++ src/views/examples/AddComponent.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+class AddComponent extends React.Component {
+
+	state = {
+		title: '',
+        salary: '',
+	}
+
+	handleChangeJobTitle = (event) => {
+        this.setState({
+            title: event.target.value,
+        })
+    }
+    
+    handleChangeSalary = (event) => {
+        this.setState({
+            salary: event.target.value,
+        })
+    }
+    
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+		if (!this.state.title || !this.state.salary) {
+			alert('Missing parameters');
+			return;
+		}
+
+        this.props.addNewJob({
+			id: Math.random(),
+			title: this.state.title,
+			salary: this.state.salary,
+		});
+
+		this.setState({
+			title: '',
+			salary: ''
+		})
+    }
+
+	render() {
+		return (
+			<form action="/action_page.php">
+				<label htmlFor="fname">First name:</label><br></br>
+				<input type="text" value={this.state.title} onChange={(event) => this.handleChangeJobTitle(event)}></input><br></br>
+				<label htmlFor="lname">Last name:</label><br></br>
+				<input type="text" value={this.state.salary} onChange={(event) => this.handleChangeSalary(event)}></input><br></br><br></br>
+				<input type="submit" value="Submit" onClick={(event) => this.handleSubmit(event)}></input>
+			</form>
+		)
+	}
+}
+
+export default AddComponent;
--- src/views/examples/MyComponent.js
+++ src/views/examples/MyComponent.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ChildComponent from './ChildComponent';
+import AddComponent from './AddComponent';
 class MyComponent extends React.Component {
 
     state = {
-        firstName: '',
-        lastName: '',
         jobs: [
             { id: 'job1', title: 'Developers', salary: '500' },
             { id: 'job2', title: 'BA', salary: '400' },
@@ -12,35 +11,16 @@ class MyComponent extends React.Component {
         ]
     }
 
-    handleChangeFirstName = (event) => {
+    addNewJob = (job) => {
         this.setState({
-            firstName: event.target.value,
+            jobs: [...this.state.jobs, job],
         })
-    }
-    
-    handleChangeLastName = (event) => {
-        this.setState({
-            lastName: event.target.value,
-        })
-    }
-    
-    handleSubmit = (event) => {
-        event.preventDefault();
-        alert('Submit');
     }
 
     render() {
         return (
             <>
-                {/* <div>Hello HTML Form</div>
-                <form action="/action_page.php">
-                    <label htmlFor="fname">First name:</label><br></br>
-                    <input type="text" value={this.state.firstName} onChange={(event) => this.handleChangeFirstName(event)}></input><br></br>
-                    <label htmlFor="lname">Last name:</label><br></br>
-                    <input type="text" value={this.state.lastName} onChange={(event) => this.handleChangeLastName(event)}></input><br></br><br></br>
-                    <input type="submit" value="Submit" onClick={(event) => this.handleSubmit(event)}></input>
-                </form>  */}
-
+                <AddComponent addNewJob={this.addNewJob}/>
                 <ChildComponent jobs={this.state.jobs} ></ChildComponent>
             </>
         )
